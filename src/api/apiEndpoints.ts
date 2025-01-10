6e2e16638ef65df146439e7ce52827a1b6101cbf
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2025-01-10 20:46:09 +0530

    feat:app.tsx deleted and apiendpoints added!

--- src/api/apiEndpoints.ts
+++ src/api/apiEndpoints.ts
@@ -2,8 +2,12 @@
 /* Backend api endpoints */
 const LOGIN_API = '/login'
 const REGISER_API = '/register'
+const GETUSER = '/get-user/:id'
+const GETVIDEOBYID = '/get-video/:id'
 
 export{
     LOGIN_API,
-    REGISER_API
+    REGISER_API,
+    GETUSER,
+    GETVIDEOBYID
 }
